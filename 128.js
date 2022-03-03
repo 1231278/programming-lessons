b36aa602448ae78a807a7676f7d26030e3f10373
Neon83 <89154590936@mail.ru>
2022-03-03 12:38:31 +0300

    маршрутизация сервера

--- 128.js
+++ 128.js
@@ -1,5 +1,18 @@
 const http = require("http");
 const requestHandler = (request, response) => {
-response.end("Hello world!");
+response.setHeader("Content-Type", "text/html; charset=utf-8;");
+if(request.url === "/home" || request.url === "/"){
+response.write("<h2>Начало</h2>");
+}
+else if(request.url == "/about"){
+response.write("<h2>Другое</h2>");
+}
+else if(request.url == "/contact"){
+response.write("<h2>Контакты</h2>");
+}
+else{
+response.write("<h2>Нет информации</h2>");
+}
+response.end();
 };
 http.createServer(requestHandler).listen(112);
