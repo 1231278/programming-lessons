f70c2cdead569a39bb9602d86942dee635b9aef7
Neon83 <89154590936@mail.ru>
2022-03-03 13:26:22 +0300

    добавлена система логирования

--- 128.js
+++ 128.js
@@ -1,18 +1,24 @@
+const fs = require("fs")
 const http = require("http");
+
 const requestHandler = (request, response) => {
 response.setHeader("Content-Type", "text/html; charset=utf-8;");
 if(request.url === "/home" || request.url === "/"){
 response.write("<h2>Начало</h2>");
+fs.appendFileSync("hello.txt",'переход в home\n' );
 }
 else if(request.url == "/about"){
 response.write("<h2>Другое</h2>");
+fs.appendFileSync("hello.txt",'переход в about\n' );
 }
 else if(request.url == "/contact"){
 response.write("<h2>Контакты</h2>");
+fs.appendFileSync("hello.txt",'переход в contact\n' );
 }
 else{
 response.write("<h2>404</h2>");
 response.write("<h2>Нет информации</h2>");
+fs.appendFileSync("hello.txt",'переход в ошибку\n' );
 }
 response.end();
 };
