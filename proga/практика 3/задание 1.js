67141fb32f42a9b90a69e9d04fe984d6dabd6550
Neon83 <89154590936@mail.ru>
2022-06-08 13:40:50 +0300

    .

--- proga/практика 3/задание 1.js
+++ proga/практика 3/задание 1.js
@@ -1,12 +1,19 @@
-app.get('/about*', function (req, res) {
-   
-})
-app.get('/about/contact', function (req, res) {
-    
-})
-app.get('/about/directions', function (req, res) {
-   
-})
+app.get('/', function (req, res) {
+    res.type('text/plain');
+    res.send('Главная страница');
+});
+app.get('/about', function (req, res) {
+    res.type('text/plain');
+    res.send('О главной странице');
+});
+// пользовательская страница 404
+app.use(function (req, res, next) {
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 — Не найдено');
+});
+
+app.use(express.static(__dirname + '/public'));
 
 
 
